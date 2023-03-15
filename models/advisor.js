2a78327c7115e5abefaeccc5b20bba5d54af0514
10xXAhmedXx10 <ahmed.salihi9610@gmail.com>
2023-03-14 23:37:53 -0600

    Update

---
+++ models/advisor.js
@@ -0,0 +1,81 @@
+const cities = [
+    {name: "California",
+    description: "California is a state located on the West Coast of the United States, known for its diverse geography, vibrant cities, and outdoor attractions. The state is home to famous destinations like Los Angeles, San Francisco, and San Diego, as well as iconic natural landmarks such as Yosemite National Park, Death Valley, and the Sierra Nevada mountain range. California offers a wide range of experiences for travelers. You can soak up the sun on the beaches of Southern California, go hiking or skiing in the mountains, explore the wine regions of Napa Valley, or take a road trip down the iconic Pacific Coast Highway.In addition to its natural beauty, California is a cultural hub with a rich history and diverse population. You can visit museums, galleries, and theaters in cities like Los Angeles and San Francisco, or explore historic sites like Alcatraz Island and the Golden Gate Bridge.Overall, California is a destination that has something for everyone, whether you're a nature lover, foodie, or culture enthusiast. So if you have the opportunity to visit, I would highly recommend it!",
+     Img:"https://dynaimage.cdn.cnn.com/cnn/q_auto,w_1275,c_fill,g_auto,h_717,ar_16:9/http%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F170606120957-california---travel-destination---shutterstock-220315747.jpg"},
+
+     {name: "Florida",
+     description:"Florida is a diverse and vibrant state located in the southeastern region of the United States. With its miles of pristine beaches, warm weather, and endless attractions, it's no wonder why it's such a popular destination for tourists from all over the world. One of the main draws of Florida is its stunning coastline. From the iconic beaches of Miami and Fort Lauderdale to the white sand beaches of the Gulf Coast, there's no shortage of beautiful spots to soak up the sun and enjoy the crystal clear waters. Visitors can swim, surf, kayak, paddleboard, or simply relax on the beach and watch the waves roll in.  Beyond the beaches, Florida has plenty of other exciting activities to offer. The state is home to several world-class theme parks, including Walt Disney World, Universal Studios, and Busch Gardens, as well as numerous water parks and family-friendly attractions. For those interested in history and culture, Florida is rich with fascinating museums, galleries, and historic sites, such as St. Augustine, the oldest continuously inhabited European-established settlement in the United States. Nature lovers will also find plenty to explore in Florida, with its vast swamps, wetlands, and wildlife preserves. The Everglades National Park is a unique ecosystem home to alligators, panthers, and other exotic wildlife, while the Florida Keys offer some of the best snorkeling and diving in the world. And of course, no trip to Florida would be complete without trying some of the state's famous cuisine, such as fresh seafood, key lime pie, and Cuban sandwiches. With so much to see and do, Florida is a top destination for any traveler looking for a fun-filled and unforgettable vacation.",
+     img:"https://a.cdn-hotels.com/gdcs/production168/d841/c65d8a45-6c0d-483c-ab77-86215443b749.jpg?impolicy=fcrop&w=1600&h=1066&q=medium"
+     },
+
+     {name: "Istanbul",
+     description:"Istanbul is a mesmerizing city that straddles both Europe and Asia, making it a unique destination that blends cultures, religions, and traditions. It is Turkey's largest city and has a rich history that spans thousands of years, evident in its stunning architecture, colorful bazaars, and vibrant street life. Istanbul is a melting pot of old and new, and visitors are sure to find something to captivate their senses. One of Istanbul's most iconic landmarks is the Hagia Sophia, a magnificent former church turned mosque turned museum that showcases a blend of Christian and Islamic architecture. Another must-visit attraction is the Blue Mosque, with its stunning blue tiles and intricate calligraphy. The Topkapi Palace, home to Ottoman sultans for centuries, is another highlight, with its beautiful gardens and impressive collections of Islamic art. Food is a major part of the Istanbul experience, and visitors can indulge in traditional Turkish cuisine like kebabs, mezze, and baklava. The city's bazaars, including the Grand Bazaar and the Spice Bazaar, are a feast for the senses, with colorful displays of spices, textiles, and ceramics. The Bosphorus, the strait that divides the city into its European and Asian sides, is also a must-see, with its stunning views and bustling boat traffic. In addition to its historical and cultural attractions, Istanbul has a vibrant nightlife scene, with bars, clubs, and restaurants catering to every taste. The city is also a shopper's paradise, with everything from high-end boutiques to street vendors selling unique souvenirs. Overall, Istanbul is a city that offers something for everyone. Its blend of history, culture, and modernity makes it a captivating destination that is sure to leave a lasting impression on any traveler.",
+     img:"https://media.timeout.com/images/105859738/750/422/image.jpg"
+     },
+
+     {name: "New Zealand",
+     description:"New Zealand is a stunningly beautiful country located in the southwestern Pacific Ocean. It is known for its breathtaking natural landscapes, rich Maori culture, and friendly locals. One of the most popular destinations in New Zealand is the South Island, home to some of the world's most spectacular scenery, including snow-capped mountains, turquoise lakes, and pristine glaciers. The North Island is equally stunning, with rugged coastlines, geothermal wonders, and lush forests. Visitors to New Zealand can also explore its vibrant cities, such as Auckland, Wellington, and Christchurch. These cities offer a mix of cosmopolitan culture, delicious cuisine, and world-class museums and galleries. New Zealand is also a haven for adventure seekers, with a wide range of thrilling activities available, such as bungee jumping, skydiving, and white-water rafting. Visitors can also go hiking, mountain biking, or skiing, depending on the season. New Zealand is famous for its wine regions, including Marlborough and Hawkes Bay, which produce some of the world's best Sauvignon Blanc and Pinot Noir. Visitors can take wine tours and enjoy tastings at the many wineries dotted throughout the country. Overall, New Zealand is a must-visit destination for anyone looking for a unique and unforgettable travel experience. Its stunning landscapes, rich culture, and adventurous spirit make it a perfect destination for travelers of all kinds. ",
+     img:"https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/LakeWakatipuNewZealand.jpg?crop=0%2C25%2C4000%2C2200&wid=2000&hei=1100&scl=2.0"
+     },
+
+     {name: "Paris",
+     description:"Paris is one of the most iconic and romantic cities in the world, known for its stunning architecture, rich history, and vibrant culture. It is the capital of France and located in the north-central part of the country. One of the main attractions of Paris is the Eiffel Tower, an iconic symbol of the city that offers panoramic views from its observation deck. Other popular tourist destinations include the Louvre Museum, which houses the famous painting of Mona Lisa, the Notre Dame Cathedral, and the Arc de Triomphe. Paris is also known for its beautiful parks and gardens, such as the Luxembourg Gardens and the Tuileries Gardens, where visitors can relax and enjoy the scenery. The Seine River runs through the heart of the city, offering boat tours and scenic walks along its banks. Foodies will delight in Paris' renowned cuisine, including its world-famous pastries, bread, cheese, and wine. Visitors can sample French delicacies at traditional bistros, cafes, and Michelin-starred restaurants. Paris is also home to some of the world's most famous fashion brands, including Chanel, Dior, and Louis Vuitton, making it a shopping destination for fashion lovers. Overall, Paris is a must-visit destination for anyone seeking to immerse themselves in the rich history, culture, and beauty of one of the world's most iconic cities. From its famous landmarks and museums to its culinary delights and fashion scene, there is something for everyone in Paris.",
+     img:"https://images.adsttc.com/media/images/5d44/14fa/284d/d1fd/3a00/003d/newsletter/eiffel-tower-in-paris-151-medium.jpg?1564742900"
+     },
+
+     {name: "London",
+     description:"London, England is a vibrant and cosmopolitan city that is full of history, culture, and entertainment. Known for its iconic landmarks, such as Big Ben, the Tower of London, and Buckingham Palace, London is a must-visit destination for anyone who wants to experience the best of British culture. One of the things that sets London apart from other cities is its incredible diversity. With more than 300 languages spoken in the city, London is a melting pot of different cultures and traditions, which is reflected in its food, music, and art scenes. From Indian curries to Chinese dim sum, from West End musicals to underground techno clubs, there's something for everyone in London. Of course, no trip to London would be complete without a visit to some of its famous museums and galleries. The British Museum, the National Gallery, and the Tate Modern are just a few of the world-class institutions that you can explore, each offering a unique insight into the history and culture of the city. When it comes to getting around, London's public transport system is one of the best in the world. The iconic red double-decker buses and black cabs are part of the city's charm, but the underground system, known as the Tube, is the most efficient way to get around. With 11 lines and over 250 stations, you can easily navigate your way around the city and discover all that London has to offer. In conclusion, London is a city that has it all – from history and culture to entertainment and diversity. Whether you're a first-time visitor or a seasoned traveler, there's always something new and exciting to discover in this dynamic and endlessly fascinating city.",
+     img:"https://www.themontcalmclub.com/blog/wp-content/uploads/2022/08/First-timers-guide-to-visiting-Big-Ben-and-Houses-of-Parliament-in-London-696x464.jpg"
+     },
+
+     {name: "",
+     description:"",
+     img:""
+     },
+
+     {name: "",
+     description:"",
+     img:""
+     },
+
+     {name: "",
+     description:"",
+     img:""
+     },
+
+     {name: "",
+     description:"",
+     img:""
+     },
+
+     {name: "",
+     description:"",
+     img:""
+     },
+
+     {name: "",
+     description:"",
+     img:""
+     },
+
+     {name: "",
+     description:"",
+     img:""
+     },
+
+     {name: "",
+     description:"",
+     img:""
+     },
+
+     {name: "",
+     description:"",
+     img:""
+     },
+
+     {name: "",
+     description:"",
+     img:""
+     },
+    ]
+module.exports = cities;
--- server.js
+++ server.js
@@ -6,13 +6,20 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.static("public"))
 
+const cities = require( "./models/advisor.js");
+
+
 app.get("/dashboard", function(req, res) {
     res.render("dashBoard.ejs")
 })
 
-app.get("/advisor", function (req, res) {
-res.render("advisor.ejs")
+app.get("/whyTravel", function(req, res){
+res.render("whyTravel.ejs")
 })
+
+app.get('/advisor', (req, res) => {
+    res.render('advisor.ejs', { cities: cities });
+});
 
 app.listen(PORT, function(req, res){
     console.log(`listening on port ${PORT}`)
